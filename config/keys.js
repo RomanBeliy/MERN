95266be7899b1a46644ab41006f1aa07ed699c19
RomanBeliy <romanpeople2292@yandex.ru>
2018-07-13 14:19:31 +0300

    add passport and protected router

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://RomanBeliy:RomanBeliy1@ds235181.mlab.com:35181/devconnectormernpublic"
+    mongoURI: "mongodb://RomanBeliy:RomanBeliy1@ds235181.mlab.com:35181/devconnectormernpublic",
+    secretOrKey: 'secret'
 };
