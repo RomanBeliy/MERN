92e728a25757173089964c3d7e5241fc2a8273a7
RomanBeliy <romanpeople2292@yandex.ru>
2018-08-09 15:19:09 +0300

    add model Profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile Model
+const Profile = require('../../models/Profile');
+// Load User Profile
+const User = require('../../models/User');
 
 //@route GET api/profile/test
 //@description Tests profile route
 //@access Public
 router.get('/test', (req, res) => res.json({msg: "Profile Works"}));
+
+//@route GET api/profile/:id
+//@description Get current users profile
+//@access Private
+router.get('/', passport.authenticate('jwt', {session: false}));
 
 module.exports = router;
